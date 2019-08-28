3b951a3674bab040fd9675709d0c16c2da4ff4de
fc836731493 <836731493@qq.com>
2019-08-28 12:06:51 +0800

    第一次上传

---
+++ src/pages/layout.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="index-wrap">
+     <div class="index-left">
+        <div class="index-left-block">
+           <h2>全部产品</h2>
+           <div v-for="product in productList">
+              <h3>{{ product.category }}</h3>
+              <ul>
+                  <li v-for="item in product.list">
+                      <a :href="item.url">{{ item.name }}</a>
+                  </li>
+              </ul>
+           </div>
+        </div>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+   name:"layout",
+   data(){
+       return{
+           productList:[
+               {
+                   category:"手机应用类",
+                   list:[
+                       {
+                           name:"91助手",
+                           url:"https://www.baidu.com/"
+                       },
+                        {
+                           name:"豌豆荚",
+                           url:"https://www.baidu.com/"
+                       },
+                        {
+                           name:"金山毒霸",
+                           url:"https://www.baidu.com/"
+                       }
+                   ]
+                },
+                {category: "开发工具",
+                   list:[
+                       {
+                           name:"WenStorm",
+                           url:"https://www.baidu.com/"
+                       },
+                        {
+                           name:"HBuiler",
+                           url:"https://www.baidu.com/"
+                       },
+                        {
+                           name:"Sublime Text 3",
+                           url:"https://www.baidu.com/"
+                       },
+                       {
+                           name:"Atom",
+                           url:"https://www.baidu.com/"
+                       }
+                   ]}                
+           ]
+       }
+   }
+}
+</script>
+
+<style scoped>
+.index-wrap {
+    width: 1200px;
+    margin: 0 auto;
+    overflow: hidden;
+}
+.index-left {
+    float: left;
+    width: 300px;
+    text-align: left;
+}
+.index-right {
+    float: left;
+    width: 900px;
+}
+.index-left-block {
+    margin: 15px;
+    background: #fff;
+    box-shadow:0 0 1px #ddd;
+    height: 350px;
+}
+.index-left-block .hr {
+    margin-bottom: 20px;
+    height: 1px;
+    width: 100%;
+    background: #ddd;
+}
+.index-left-block h2 {
+    background: #4fc08d;
+    color:#fff;
+    padding: 10px 15px;
+    margin-bottom: 20px;
+}
+.index-left-block h3 {
+    padding: 0 15px 5px 15px;
+    font-weight: bold;
+    color: #222;
+}
+.index-left-block ul {
+    padding: 10px 15px;
+}
+.index-left-block li {
+    padding: 5px;
+}
+.index-board-list {
+    overflow: hidden;
+}
+.index-board-item {
+    float: left;
+    width: 400px;
+    background: #fff;
+    box-shadow: 0 0 1px #ddd;
+    padding: 20px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+}
+.index-board-item-inner {
+    min-height: 125px;
+    padding-left: 120px;
+}
+.index-board-car .index-board-item-inner {
+    background: url(../assets/images/1.png) no-repeat;
+}
+.index-board-loud .index-board-item-inner {
+    background: url(../assets/images/2.png) no-repeat;
+}
+.index-board-earth .index-board-item-inner {
+    background: url(../assets/images/3.png) no-repeat;    
+}
+.index-board-hill .index-board-item-inner {
+    background: url(../assets/images/4.png) no-repeat;    
+}
+.index-board-item h2 {
+    font-size: 18px;
+    font-weight: bold;
+    color: #000;
+    margin-bottom: 15px;
+}
+.line-last {
+    margin-right: 0;
+}
+.index-board-button {
+    margin-top: 20px;
+}
+.lastest-news {
+    min-height: 350px;
+}
+.hot-tag {
+    background: red;
+    color: #fff;
+}
+.new-item {
+    display: inline-block;
+    width: 230px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+</style>
